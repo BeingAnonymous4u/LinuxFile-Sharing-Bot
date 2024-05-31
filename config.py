be0a91078154f910b738b53e9adf2c46c14ce841
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-05-31 15:00:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,14 +49,12 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", """<b><a href="https://t.me/+234Q4gdwd9BhNDU1">📝Fɪʟᴇ Nᴀᴍᴇ :  {filename}
-
-<a href="https://t.me/iPopCormbot"><i>ᴘʟᴇᴀsᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪs ꜰɪʟᴇs ᴛᴏ ᴡᴀᴛᴄʜ ᴀɴᴅ sᴛʀᴇᴀᴍ ᴛʜɪs ғɪʟᴇ</i></a></b>""")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
+#Set true if you want Disable your Channel Posts Share button 
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
