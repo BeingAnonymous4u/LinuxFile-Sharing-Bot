f5ef4b566ca19d0df12739791bfadf57bdb15644
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-05-31 18:39:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,3 @@
 class script(object):
 
-    CUSTOM_CAPTION = """<b>[📝Fɪʟᴇ Nᴀᴍᴇ :  {filename}](https://t.me/+234Q4gdwd9BhNDU1) \n\n<i>[ᴘʟᴇᴀsᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪs ꜰɪʟᴇs ᴛᴏ ᴡᴀᴛᴄʜ ᴀɴᴅ sᴛʀᴇᴀᴍ ᴛʜɪs ғɪʟᴇ](https://t.me/iPopCormbot)</i></b>"""
+    CAPTION = """<b>[📝Fɪʟᴇ Nᴀᴍᴇ :  {filename}](https://t.me/+234Q4gdwd9BhNDU1) \n\n<i>[ᴘʟᴇᴀsᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪs ꜰɪʟᴇs ᴛᴏ ᴡᴀᴛᴄʜ ᴀɴᴅ sᴛʀᴇᴀᴍ ᴛʜɪs ғɪʟᴇ](https://t.me/iPopCormbot)</i></b>"""
