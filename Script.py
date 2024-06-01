ef4a7df055a7868aa208ba2c3c2c9734a03b9a9c
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-06-01 19:09:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,6 @@
 class script(object):
 
-    CAPTION = """<b>[📝Fɪʟᴇ Nᴀᴍᴇ :  {filename}](https://t.me/+234Q4gdwd9BhNDU1) \n\n<i>[ᴘʟᴇᴀsᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪs ꜰɪʟᴇs ᴛᴏ ᴡᴀᴛᴄʜ ᴀɴᴅ sᴛʀᴇᴀᴍ ᴛʜɪs ғɪʟᴇ](https://t.me/iPopCormbot)</i></b>"""
+    CAPTION = """📝Fɪʟᴇ Nᴀᴍᴇ :  {filename}
+⚙️ sɪᴢᴇ : {file_size}
+
+ᴘʟᴇᴀsᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪs ꜰɪʟᴇs ᴛᴏ ᴛʜᴇ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇ ᴀɴᴅ ᴄʟᴏsᴇ ᴛʜɪs ᴍᴇssᴀɢᴇ"""
